dbe56e39fe104c5c1ceb2de25d0947c267424c70
D2005escobar10 <escobardavid2022@gmail.com>
2024-09-21 18:08:26 -0600

    Add files via upload

---
+++ TallerMejoraInterfaces/Menu.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace BibliotecaHerecia
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            PanelMenuLateral = new Panel();
+            pnlsubmenu3 = new Panel();
+            btnprestamolibro = new Button();
+            btnpresamo = new Button();
+            pnlsubmenu2 = new Panel();
+            btnHistorialLibros = new Button();
+            btnagregarlibros = new Button();
+            btnAdministrarLibros = new Button();
+            pnlsubmenu1 = new Panel();
+            btnHistorialMiembros = new Button();
+            button2 = new Button();
+            btnagregarmiembros = new Button();
+            btnadministrarmiembros = new Button();
+            pictureBox1 = new PictureBox();
+            PanelMenuLateral.SuspendLayout();
+            pnlsubmenu3.SuspendLayout();
+            pnlsubmenu2.SuspendLayout();
+            pnlsubmenu1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // PanelMenuLateral
+            // 
+            PanelMenuLateral.AutoScroll = true;
+            PanelMenuLateral.BackColor = Color.FromArgb(0, 64, 64);
+            PanelMenuLateral.Controls.Add(pnlsubmenu3);
+            PanelMenuLateral.Controls.Add(btnpresamo);
+            PanelMenuLateral.Controls.Add(pnlsubmenu2);
+            PanelMenuLateral.Controls.Add(btnAdministrarLibros);
+            PanelMenuLateral.Controls.Add(pnlsubmenu1);
+            PanelMenuLateral.Controls.Add(btnadministrarmiembros);
+            PanelMenuLateral.Dock = DockStyle.Left;
+            PanelMenuLateral.Location = new Point(0, 0);
+            PanelMenuLateral.Name = "PanelMenuLateral";
+            PanelMenuLateral.Size = new Size(250, 592);
+            PanelMenuLateral.TabIndex = 14;
+            // 
+            // pnlsubmenu3
+            // 
+            pnlsubmenu3.BackColor = Color.Teal;
+            pnlsubmenu3.Controls.Add(btnprestamolibro);
+            pnlsubmenu3.Dock = DockStyle.Top;
+            pnlsubmenu3.Location = new Point(0, 245);
+            pnlsubmenu3.Name = "pnlsubmenu3";
+            pnlsubmenu3.Size = new Size(250, 47);
+            pnlsubmenu3.TabIndex = 4;
+            // 
+            // btnprestamolibro
+            // 
+            btnprestamolibro.FlatAppearance.BorderSize = 0;
+            btnprestamolibro.FlatAppearance.MouseDownBackColor = Color.Silver;
+            btnprestamolibro.FlatStyle = FlatStyle.Flat;
+            btnprestamolibro.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnprestamolibro.ForeColor = Color.FromArgb(255, 255, 192);
+            btnprestamolibro.Location = new Point(0, 9);
+            btnprestamolibro.Name = "btnprestamolibro";
+            btnprestamolibro.Padding = new Padding(35, 0, 0, 0);
+            btnprestamolibro.Size = new Size(250, 29);
+            btnprestamolibro.TabIndex = 3;
+            btnprestamolibro.Text = "Realiza  Tu Prestamo";
+            btnprestamolibro.TextAlign = ContentAlignment.MiddleLeft;
+            btnprestamolibro.UseVisualStyleBackColor = true;
+            btnprestamolibro.Click += btnprestamolibro_Click;
+            // 
+            // btnpresamo
+            // 
+            btnpresamo.Dock = DockStyle.Top;
+            btnpresamo.FlatAppearance.BorderSize = 0;
+            btnpresamo.FlatStyle = FlatStyle.Flat;
+            btnpresamo.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnpresamo.ForeColor = Color.White;
+            btnpresamo.Location = new Point(0, 216);
+            btnpresamo.Name = "btnpresamo";
+            btnpresamo.Padding = new Padding(10, 0, 0, 0);
+            btnpresamo.Size = new Size(250, 29);
+            btnpresamo.TabIndex = 3;
+            btnpresamo.Text = "Realizar Prestamo";
+            btnpresamo.TextAlign = ContentAlignment.MiddleLeft;
+            btnpresamo.UseVisualStyleBackColor = true;
+            btnpresamo.Click += btnpresamo_Click;
+            // 
+            // pnlsubmenu2
+            // 
+            pnlsubmenu2.BackColor = Color.Teal;
+            pnlsubmenu2.Controls.Add(btnHistorialLibros);
+            pnlsubmenu2.Controls.Add(btnagregarlibros);
+            pnlsubmenu2.Dock = DockStyle.Top;
+            pnlsubmenu2.Location = new Point(0, 133);
+            pnlsubmenu2.Name = "pnlsubmenu2";
+            pnlsubmenu2.Size = new Size(250, 83);
+            pnlsubmenu2.TabIndex = 2;
+            // 
+            // btnHistorialLibros
+            // 
+            btnHistorialLibros.FlatAppearance.BorderSize = 0;
+            btnHistorialLibros.FlatAppearance.MouseDownBackColor = Color.Silver;
+            btnHistorialLibros.FlatStyle = FlatStyle.Flat;
+            btnHistorialLibros.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHistorialLibros.ForeColor = Color.FromArgb(255, 255, 192);
+            btnHistorialLibros.Location = new Point(0, 7);
+            btnHistorialLibros.Name = "btnHistorialLibros";
+            btnHistorialLibros.Padding = new Padding(35, 0, 0, 0);
+            btnHistorialLibros.Size = new Size(250, 29);
+            btnHistorialLibros.TabIndex = 3;
+            btnHistorialLibros.Text = "Historial De Libros";
+            btnHistorialLibros.TextAlign = ContentAlignment.MiddleLeft;
+            btnHistorialLibros.UseVisualStyleBackColor = true;
+            btnHistorialLibros.Click += button4_Click;
+            // 
+            // btnagregarlibros
+            // 
+            btnagregarlibros.FlatAppearance.BorderSize = 0;
+            btnagregarlibros.FlatAppearance.MouseDownBackColor = Color.Silver;
+            btnagregarlibros.FlatStyle = FlatStyle.Flat;
+            btnagregarlibros.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnagregarlibros.ForeColor = Color.FromArgb(255, 255, 192);
+            btnagregarlibros.Location = new Point(0, 42);
+            btnagregarlibros.Name = "btnagregarlibros";
+            btnagregarlibros.Padding = new Padding(35, 0, 0, 0);
+            btnagregarlibros.Size = new Size(250, 29);
+            btnagregarlibros.TabIndex = 2;
+            btnagregarlibros.Text = "Agregar Libros";
+            btnagregarlibros.TextAlign = ContentAlignment.MiddleLeft;
+            btnagregarlibros.UseVisualStyleBackColor = true;
+            btnagregarlibros.Click += button3_Click;
+            // 
+            // btnAdministrarLibros
+            // 
+            btnAdministrarLibros.Dock = DockStyle.Top;
+            btnAdministrarLibros.FlatAppearance.BorderSize = 0;
+            btnAdministrarLibros.FlatStyle = FlatStyle.Flat;
+            btnAdministrarLibros.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdministrarLibros.ForeColor = Color.White;
+            btnAdministrarLibros.Location = new Point(0, 104);
+            btnAdministrarLibros.Name = "btnAdministrarLibros";
+            btnAdministrarLibros.Padding = new Padding(10, 0, 0, 0);
+            btnAdministrarLibros.Size = new Size(250, 29);
+            btnAdministrarLibros.TabIndex = 2;
+            btnAdministrarLibros.Text = "Administración De Libros";
+            btnAdministrarLibros.TextAlign = ContentAlignment.MiddleLeft;
+            btnAdministrarLibros.UseVisualStyleBackColor = true;
+            btnAdministrarLibros.Click += btnAdministrarLibros_Click;
+            // 
+            // pnlsubmenu1
+            // 
+            pnlsubmenu1.BackColor = Color.Teal;
+            pnlsubmenu1.Controls.Add(btnHistorialMiembros);
+            pnlsubmenu1.Controls.Add(button2);
+            pnlsubmenu1.Controls.Add(btnagregarmiembros);
+            pnlsubmenu1.Dock = DockStyle.Top;
+            pnlsubmenu1.Location = new Point(0, 29);
+            pnlsubmenu1.Name = "pnlsubmenu1";
+            pnlsubmenu1.Size = new Size(250, 75);
+            pnlsubmenu1.TabIndex = 1;
+            // 
+            // btnHistorialMiembros
+            // 
+            btnHistorialMiembros.FlatAppearance.BorderSize = 0;
+            btnHistorialMiembros.FlatAppearance.MouseDownBackColor = Color.Silver;
+            btnHistorialMiembros.FlatStyle = FlatStyle.Flat;
+            btnHistorialMiembros.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHistorialMiembros.ForeColor = Color.FromArgb(255, 255, 192);
+            btnHistorialMiembros.Location = new Point(0, 6);
+            btnHistorialMiembros.Name = "btnHistorialMiembros";
+            btnHistorialMiembros.Padding = new Padding(35, 0, 0, 0);
+            btnHistorialMiembros.Size = new Size(250, 29);
+            btnHistorialMiembros.TabIndex = 2;
+            btnHistorialMiembros.Text = "Historial De Miembros";
+            btnHistorialMiembros.TextAlign = ContentAlignment.MiddleLeft;
+            btnHistorialMiembros.UseVisualStyleBackColor = true;
+            btnHistorialMiembros.Click += button3_Click_1;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.Silver;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(0, 75);
+            button2.Name = "button2";
+            button2.Padding = new Padding(35, 0, 0, 0);
+            button2.Size = new Size(250, 29);
+            button2.TabIndex = 1;
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // btnagregarmiembros
+            // 
+            btnagregarmiembros.FlatAppearance.BorderSize = 0;
+            btnagregarmiembros.FlatAppearance.MouseDownBackColor = Color.Silver;
+            btnagregarmiembros.FlatStyle = FlatStyle.Flat;
+            btnagregarmiembros.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnagregarmiembros.ForeColor = Color.FromArgb(255, 255, 192);
+            btnagregarmiembros.Location = new Point(0, 38);
+            btnagregarmiembros.Name = "btnagregarmiembros";
+            btnagregarmiembros.Padding = new Padding(35, 0, 0, 0);
+            btnagregarmiembros.Size = new Size(250, 29);
+            btnagregarmiembros.TabIndex = 0;
+            btnagregarmiembros.Text = "Agregar Miembros";
+            btnagregarmiembros.TextAlign = ContentAlignment.MiddleLeft;
+            btnagregarmiembros.UseVisualStyleBackColor = true;
+            btnagregarmiembros.Click += button2_Click;
+            // 
+            // btnadministrarmiembros
+            // 
+            btnadministrarmiembros.Dock = DockStyle.Top;
+            btnadministrarmiembros.FlatAppearance.BorderSize = 0;
+            btnadministrarmiembros.FlatStyle = FlatStyle.Flat;
+            btnadministrarmiembros.Font = new Font("Showcard Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnadministrarmiembros.ForeColor = Color.White;
+            btnadministrarmiembros.Location = new Point(0, 0);
+            btnadministrarmiembros.Name = "btnadministrarmiembros";
+            btnadministrarmiembros.Padding = new Padding(10, 0, 0, 0);
+            btnadministrarmiembros.Size = new Size(250, 29);
+            btnadministrarmiembros.TabIndex = 0;
+            btnadministrarmiembros.Text = "Administración De Miembros";
+            btnadministrarmiembros.TextAlign = ContentAlignment.MiddleLeft;
+            btnadministrarmiembros.UseVisualStyleBackColor = true;
+            btnadministrarmiembros.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(428, 104);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(443, 378);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(1028, 592);
+            Controls.Add(pictureBox1);
+            Controls.Add(PanelMenuLateral);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Menu";
+            Text = "Menu";
+            PanelMenuLateral.ResumeLayout(false);
+            pnlsubmenu3.ResumeLayout(false);
+            pnlsubmenu2.ResumeLayout(false);
+            pnlsubmenu1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel PanelMenuLateral;
+        private Button btnadministrarmiembros;
+        private Panel pnlsubmenu1;
+        private Button btnagregarmiembros;
+        private Button btnAdministrarLibros;
+        private Button btnagregarlibros;
+        private Button button2;
+        private Panel pnlsubmenu2;
+        private Button btnHistorialMiembros;
+        private Button btnHistorialLibros;
+        private Button btnpresamo;
+        private Panel pnlsubmenu3;
+        private Button btnprestamolibro;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ TallerMejoraInterfaces/Menu.cs
@@ -0,0 +1,109 @@
+﻿using BibliotecaHerecia.Miembros;
+using BibliotecaHerecia.Libros;
+using BibliotecaHerecia.Prestamos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BibliotecaHerecia
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            CustomizeDesing();
+        }
+
+        private void CustomizeDesing()
+        {
+            pnlsubmenu1.Visible = false;
+            pnlsubmenu2.Visible = false;
+            pnlsubmenu3.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (pnlsubmenu1.Visible == true)
+                pnlsubmenu1.Visible = false;
+            if (pnlsubmenu2.Visible == true)
+                pnlsubmenu2.Visible = false;
+            if (pnlsubmenu3.Visible == true)
+                pnlsubmenu3.Visible = false;
+        }
+
+        private void ShowSubMenu(Panel SubMenu)
+        {
+            if (SubMenu.Visible == false)
+            {
+                hideSubMenu();
+                SubMenu.Visible = true;
+            }
+            else
+            {
+                SubMenu.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(pnlsubmenu1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NuevoMiembro nuevoMiembro = new NuevoMiembro();
+            nuevoMiembro.ShowDialog();
+            hideSubMenu();
+        }
+
+        private void btnAdministrarLibros_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(pnlsubmenu2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            NuevoLibro nuevoLibro = new NuevoLibro();
+            nuevoLibro.ShowDialog();
+            hideSubMenu();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Form verMiembrosForm = new VerMiembros();
+            verMiembrosForm.Show();
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form verLibrosForm = new VerLibros();
+            verLibrosForm.Show();
+            hideSubMenu();
+        }
+
+        private void btnprestamolibro_Click(object sender, EventArgs e)
+        {
+            Form realizarPrestamoForm = new RealizarPrestamo();
+            realizarPrestamoForm.Show();
+            hideSubMenu();
+        }
+
+        private void btnpresamo_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(pnlsubmenu3);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
